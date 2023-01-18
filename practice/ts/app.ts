0074c757b986455388f5f2f004c02988d0104ca4
binslog <bins95@naver.com>
2023-01-18 17:56:01 +0900

    20230118 ssafy

---
+++ practice/ts/app.ts
@@ -0,0 +1,16 @@
+class Employee {
+  fullName: string;
+  age: number;
+  jobTitle: string;
+  hourlyRate: number;
+  workingHoursPerWeek: number;
+
+  printEmployeeDetails = (): void => {
+    console.log(`${this.fullName}의 직업은 ${this.jobTitle}이고
+    일주일의 수입은 ${this.hourlyRate * this.workingHoursPerWeek} 달러이다.
+  `);
+  };
+}
+
+let employee1 = new Employee(); //
+employee1.printEmployeeDetails();
